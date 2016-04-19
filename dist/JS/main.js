0a7436996f65a33cfbbd3de2b27b26f947aac2da
Liz Swain <swainea@gmail.com>
2016-04-18 20:06:20 -0400

    appended elements to repoDetails UI

--- dist/JS/main.js
+++ dist/JS/main.js
@@ -111,16 +111,23 @@ ns.init = function() {
 
     ns.renderRepoDetails = function renderRepoDetails( repoData ){
       $('#reposDetail').empty();
-       console.log('Inside Repo Details');
+      //  console.log('Inside Repo Details');
        $( '#reposDetail')
-           .append( $('p').text( repoData.forks));
+           .append($('<a href= "REPLACE_ME" >' + repoData.name + '</a>')) //the url should be dot notation
+           .append( $('<p>').text( repoData.description )) // p tag not working
+           .append($('<a href= "REPLACE_ME" >' + repoData.open_issues_count + ' open issues </a>')) //fix URL
+           .append( $('<ul>')
+              .append($('<li>').text( 'Owner: ' + repoData.owner.login ))
+              .append($('<li>').text( 'Stars: ' + repoData.stargazers_count ))
+              .append($('<li>').text( 'Forks: ' + repoData.forks ))
+              .append($('<li>').text( 'Created On: ' + repoData.created_at ))
+            );
     };
 
     ns.repoDetailNav = function repoDetailNav( repoData ){
-      //nav tab is being duplicated each time the page loads 
+      //nav tab is being duplicated each time the page loads
 
       // console.log('Inside Repo Detail Nav');
-      // console.log('Repo Data reaching Nav:', repoData);
       $('.nav')
           .append($('<li>')
              .append($('<a href="#reposDetail__' + repoData.name + ' class="active" ">' + 'Repo Detail' + '</a>') //try also adding the aria if this works
